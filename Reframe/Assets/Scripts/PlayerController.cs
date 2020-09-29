ecb16596fc87cd5513af22d13124082e3161ca44
rcsskyline <71791272+rcsskyline@users.noreply.github.com>
2020-09-28 23:19:09 -0500

    Boundaries added and Player movement created

--- Reframe/Assets/Scripts/PlayerController.cs
+++ Reframe/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D players;
+
+    Vector2 movement;
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        players.MovePosition(players.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
